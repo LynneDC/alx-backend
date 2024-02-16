e368c3d5466beef4cc6951430e0c5358876e4cb7
LynneDC <roselinedc1308@gmail.com>
2024-02-16 16:21:48 +0200

    done

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Task 1: Simple pagination.
+
+This module provides a Server class that can be used to paginate a database of popular baby names.
+It also includes a function to retrieve the index range from a given page and page size.
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+
+    Args:
+        page (int): The page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple representing the start and end index of the page.
+    """
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+
+        Returns:
+            List[List]: The dataset containing the popular baby names.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+
+        Args:
+            page (int, optional): The page number. Defaults to 1.
+            page_size (int, optional): The number of items per page. Defaults to 10.
+
+        Returns:
+            List[List]: A list of rows representing the data on the requested page.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
