8b131c53c3d0e22e11da6f5f82d62ac9776ee933
LynneDC <roselinedc1308@gmail.com>
2024-02-17 08:09:52 +0200

    fix merge

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -55,7 +55,6 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-<<<<<<< HEAD
         """return the value in self.cache_data linked to key
 
         Args:
@@ -64,8 +63,6 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
-=======
         '''return the value in `self.cache_data` linked to `key`
         '''
         return self.cache_data.get(key, None)
->>>>>>> fa0c5efa2872370a171b9c2ba833f6cee192e224
